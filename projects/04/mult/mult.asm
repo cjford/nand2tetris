e2dcda1f44c52d3bfb0e95ad8c6eb159f7daf9cf
Connor Ford <git@cjford.net>
2017-02-06 15:39:12 -0700

    Add mult program

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Clear previous result
+@R2
+M=0
+
+// Add the value of R1 to value of R2, and do it as many times as the value of R0
+(LOOP)
+  // Terminate when R0 reaches 0
+  @R0
+  D=M
+  @END
+  D;JEQ
+
+  // Increase result value by value of R1
+  @R1
+  D=M
+  @R2
+  M=M+D
+
+  // Decrement R0
+  @R0
+  M=M-1
+
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
